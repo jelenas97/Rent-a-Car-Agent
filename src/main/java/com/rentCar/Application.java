64c7b33d618354f884657f4ff51fcb68b1cd17d3
jovica.josipovic97@gmail.com <jovica.josipovic97@gmail.com>
2020-05-03 23:37:38 +0200

    Searching avertisements done

--- src/main/java/com/rentCar/Application.java
+++ src/main/java/com/rentCar/Application.java
@@ -10,16 +10,16 @@ import java.time.Period;
 public class Application {
     public static void main(String[] args){
 
-        LocalDate startDate = LocalDate.of(2017, 2, 20);
-        LocalDate endDate = LocalDate.of(2017, 1, 15);
-
-        Period period = Period.between(startDate, endDate);
+//        LocalDate startDate = LocalDate.of(2017, 2, 20);
+//        LocalDate endDate = LocalDate.of(2017, 1, 15);
+//
+//        Period period = Period.between(startDate, endDate);
         
         SpringApplication.run(Application.class,args);
 
 
     }
-    public boolean isOverlapping(LocalDate start1, LocalDate end1, LocalDate start2, LocalDate end2) {
-        return !start1.isAfter(end2) && !start2.isAfter(end1);
-    }
+//    public boolean isOverlapping(LocalDate start1, LocalDate end1, LocalDate start2, LocalDate end2) {
+//        return !start1.isAfter(end2) && !start2.isAfter(end1);
+//    }
 }
