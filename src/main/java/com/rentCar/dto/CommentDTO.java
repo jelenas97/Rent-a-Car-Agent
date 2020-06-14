1af7eb2e874ec758eb3129390cef881e3a5dfa9f
SaraFojkar <56953463+SaraFojkar@users.noreply.github.com>
2020-06-14 18:23:02 +0200

    updated comment and rate DTOs

--- src/main/java/com/rentCar/dto/CommentDTO.java
+++ src/main/java/com/rentCar/dto/CommentDTO.java
@@ -12,20 +12,24 @@ import java.time.LocalDate;
 public class CommentDTO {
     private Long id;
     private LocalDate date;
+    private String dateString;
     private String content;
     private String status;
     private Long advertisement_id;
     private Long client_id;
+    private String client;
     private Long rent_request_id;
 
 
     public CommentDTO(Comment comment){
         this.id = comment.getId();
         this.date = comment.getDate();
+        this.dateString=comment.getDate().toString();
         this.content = comment.getContent();
         this.status = comment.getStatus().toString();
         this.advertisement_id = comment.getAdvertisement().getId();
         this.client_id = comment.getUser().getId();
+        this.client= comment.getUser().getFirstName()+" "+ comment.getUser().getLastName();
         this.rent_request_id= comment.getRentRequest().getId();
     }
 }
