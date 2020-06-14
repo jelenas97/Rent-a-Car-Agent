e61ead4228bc959f1be38a1b5ee131260b5766fe
SaraFojkar <56953463+SaraFojkar@users.noreply.github.com>
2020-06-14 18:25:04 +0200

    updated service and rate service and serviceImpl

--- src/main/java/com/rentCar/service/RateService.java
+++ src/main/java/com/rentCar/service/RateService.java
@@ -2,7 +2,10 @@ package com.rentCar.service;
 
 import com.rentCar.dto.RateDTO;
 
+import java.util.List;
+
 public interface RateService {
 
     long rateAdvertisement(RateDTO dto);
+    List<RateDTO> findAverageAdvRate(long id);
 }
