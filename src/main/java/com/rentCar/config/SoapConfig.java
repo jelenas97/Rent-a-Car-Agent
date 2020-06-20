9f9bc010e94809266548b62e254d917a32f2d421
petarcurcin <petar.curcin97@gmail.com>
2020-06-20 13:37:33 +0200

    soap configuration

--- src/main/java/com/rentCar/config/SoapConfig.java
+++ src/main/java/com/rentCar/config/SoapConfig.java
@@ -1,6 +1,7 @@
 package com.rentCar.config;
 
 import com.rentCar.soap.AdClient;
+import com.rentCar.soap.MessageClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
@@ -22,6 +23,15 @@ public class SoapConfig {
     public AdClient adClient(Jaxb2Marshaller marshaller) {
         AdClient client = new AdClient();
         client.setDefaultUri("http://localhost:8084/advertisement/ws");
+        client.setMarshaller(marshaller);
+        client.setUnmarshaller(marshaller);
+        return client;
+    }
+
+    @Bean
+    public MessageClient messageClient(Jaxb2Marshaller marshaller) {
+        MessageClient client = new MessageClient();
+        client.setDefaultUri("http://localhost:8087/microservices/messages/ws");
         client.setMarshaller(marshaller);
         client.setUnmarshaller(marshaller);
         return client;
