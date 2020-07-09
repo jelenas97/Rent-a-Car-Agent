0ecf40b0d3129280ff8dde027dce888d8b511e9a
jovica.josipovic97@gmail.com <jovica.josipovic97@gmail.com>
2020-07-09 17:39:12 +0200

    Changes on ordinary renting (automatic reject) and logic moved in service

--- src/main/java/com/rentCar/repository/RequestsHolderRepository.java
+++ src/main/java/com/rentCar/repository/RequestsHolderRepository.java
@@ -11,4 +11,7 @@ import java.util.List;
 public interface RequestsHolderRepository extends JpaRepository<com.rentCar.model.RequestsHolder, Long> {
     @Query(value = "select c from RequestsHolder c inner join c.rentRequests req inner join req.advertisement a where req.rentRequestStatus='PENDING' and a.owner.id=?1 group by c.id")
     List<RequestsHolder> getAllPending(Long id);
+
+    @Query(value = "select c from RequestsHolder c where  c.id = ?1")
+    RequestsHolder find(Long id);
 }
